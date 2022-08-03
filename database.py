67c7eb36a4a50a58b1053ba2850431e3cf46cc7e
1Tsupryk <tsuprykdmytro@gmail.com>
2022-08-03 17:26:56 +0300

    databaseFix#3

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ class Database:
     def __init__(self):
         self.balance = 100
         self.password = ""
-        self.current_category = []
         self.new_ID = {}
         self.cpu = []
         self.ram = []
@@ -14,33 +13,33 @@ class Database:
 
     def add_product(self, category_name, characteristics):
         """Adding new product to the list of certain category"""
-        self.current_category = self.get_products_list(category_name)
-        self.new_ID = {"ID":len(self.current_category)}
-        self.current_category.append({**self.new_ID, **characteristics})
+        current_category = self.get_products_list(category_name)
+        self.new_ID = {"ID":len(current_category)}
+        current_category.append({**self.new_ID, **characteristics})
 
     def delete_product(self, category_name, id):
         """Removing a specific product from the list"""
-        self.current_category = self.get_products_list(category_name)
-        del self.current_category[id]
+        current_category = self.get_products_list(category_name)
+        del current_category[id]
         try:
-            for product in self.current_category[id:]:
+            for product in current_category[id:]:
                 product["ID"] -= 1
         except IndexError:
             pass
     
     def update_product(self, category_name, id, changes):
         """Updating a specific product from the list"""
-        self.current_category = self.get_products_list(category_name)
+        current_category = self.get_products_list(category_name)
         self.new_ID = {"ID":id}  
-        self.current_category[id] = {**self.new_ID, **changes}
+        current_category[id] = {**self.new_ID, **changes}
 
     def buy_product(self, category_name, id, number):
         """Reduces the balance and quantity of the product"""
-        self.current_category = self.get_products_list(category_name)
-        self.balance -= self.current_category[id]["Price"]*number
-        self.current_category[id]["Number"] -= number
-        if self.current_category[id]["Number"] == 0:
-            self.DELETE(self.current_category, id)
+        current_category = self.get_products_list(category_name)
+        self.balance -= current_category[id]["Price"]*number
+        current_category[id]["Number"] -= number
+        if current_category[id]["Number"] == 0:
+            self.DELETE(current_category, id)
 
     def get_products_list(self, category_name):
         return getattr(self, category_name)
