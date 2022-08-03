c5408ce6f8d219983127d4deef742221e7f5395d
1Tsupryk <tsuprykdmytro@gmail.com>
2022-08-03 20:07:02 +0300

    consoleFix#1

--- console.py
+++ console.py
@@ -2,29 +2,19 @@ import re
 
 class Console:
 
-    def __init__(self):
-        self.category = ""
-        self.action = ""
-        self.category_name = ""
-        self.password = ""
-        self.asked_password = ""
-        self.current_position = ""
-        self.db_action = ""
-        self.chars = {}
-        self.id = 0
-        self.number = 0
-
     def read_user_action(self):
         """Reads user's action right after program starts""" 
-        self.current_position = "start"
         print("-"*50)       
         print(("Enter B to check your balance.\n"
         "Enter C to choose category.\n"
         "Enter A to sign in as Admin.\n"
         "Enter Q to quit the shop."))
         print("-"*50)
-        self.action = input("Please, choose the option: ").strip().lower()
-        self.__is_valid_user_action(self.action)
+        action = input("Please, choose the option: ").strip().lower()
+        if self.__is_valid_user_action(action):
+            return action
+        else:
+            self.read_user_action()
     
     def __is_valid_user_action(self, user_action):
         """User action validation"""        
@@ -32,14 +22,12 @@ class Console:
         and user_action != "q"):
             print("-"*50)
             print("Input is invalid, try again.")
-            self.read_user_action()
-
-    def get_category(self, db):
-        """Reads users entered category and saves it to the variable"""
-        if self.current_position == "db_action":
-            self.current_position = "db_category_choosing"
+            return False
         else:
-            self.current_position = "category_choosing"
+            return True
+
+    def get_category(self):
+        """Reads users entered category and saves it to the variable"""
         print("-"*50)
         print(("Categories: \nEnter CPU for CPUs/Processors; \n"
         "Enter RAM for Memory (RAM); \n"
@@ -49,23 +37,24 @@ class Console:
         "Enter FAN for Fans, Heat Sinks & Cooling. \n"
         "Enter R to return."))
         print("-"*50)
-        self.category_name = input("Please, choose the option: ").strip().lower()
-        if self.category_name == "r":
-            self.return_back(self.current_position)
+        category_name = input("Please, choose the option: ").strip().lower()
+        if category_name == "r":
+            return "r"
+        elif self.__is_valid_category(category_name):
+            return category_name
         else:
-            self.__is_valid_category(self.category_name)
-            self.category = getattr(db, self.category_name)
-
+            self.get_category()
+        
     def __is_valid_category(self, category_name):
         """Category input validation"""        
-        if (category_name != "ram" and category_name != "cpu" and category_name != "power"
-        and category_name != "motherboard" and category_name != "video" and category_name != "fan"):
+        if (category_name != "ram" and category_name != "cpu" 
+        and category_name != "power" and category_name != "motherboard"
+        and category_name != "video" and category_name != "fan"):
             print("-"*50)
-            self.category_name = input("Input is invalid, try again: ")
-            if self.category_name == "r":
-                self.return_back(self.current_position)
-            else:
-                self.__is_valid_category(self.category_name)
+            print("Input is invalid, try again.")
+            return False
+        else:
+            return True
 
     def show_balance(self, balance):
         """Outputs users balance"""        
@@ -74,86 +63,88 @@ class Console:
         print(f"Your balance: {balance}$")
         self.return_back(self.current_position)
 
-    def password_creation(self, db):
+    def create_password(self):
         """Creates password for admin panel"""        
-        self.current_position = "password_creation"
         print("-"*50)
-        self.password = input(("You are entering as admin for the first time. \n"
-        "Please, create the password (at least 8 characters) or enter R to return: "))
-        if self.password == "r":
-            self.return_back(self.current_position)
-        else:
-            self.__is_valid_password(self.password, db)
-
-    def __is_valid_password(self, password, db):
-        """Validation of entered password"""        
-        if re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
-            db.password = password
+        password = input("Please, create the password (at least 8 characters) or enter R to return: ")
+        if password.lower().strip() == "r":
+            return "r"
+        elif self.__is_valid_password(password):
             print("-"*50)
             print("Password saved!")
+            return password
+        else:
+            self.create_password()
+
+
+    def __is_valid_password(self, password):
+        """Validation of entered password"""        
+        if re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
+            return True
         else:
             print("-"*50)
-            password = input("Password is invalid, try again: ")
-            if password == "r":
-                self.return_back(self.current_position)
-            else:
-                self.__is_valid_password(password, db)
+            print("Password is invalid, try again.")
+            return False
 
-    def __is_password_correct(self, asked_password, password):
+    def login(self, password):
+        """Asking for password to log in admin panel """        
+        print("-"*50)
+        login_password = input("Please, enter the password or enter R to return.\n")
+        is_password_correct = self.__login_check(login_password, password)
+        if is_password_correct:
+            if is_password_correct == "r":
+                return "r"
+            else:
+                return True
+        else:
+            self.login(password)
+
+    def __login_check(self, login_password, password):
         """Checking the correctness of the password"""        
-        if asked_password == password:
+        if login_password == password:
             print("-"*50)
             print("Password confirmed!")
-        elif asked_password.lower() == "r":
-            self.read_user_action()
+            return True
+        elif login_password.lower() == "r":
+            return "r"
         else:
             print("-"*50)
-            asked_password = input("Wrong password, try again: \n")
-            self.__is_password_correct(asked_password, password)
-
-    def ask_for_password(self, password):
-        """Asking for password to log in admin panel """        
-        self.current_position = "password_check"
-        print("-"*50)
-        self.asked_password = input("Please, enter the password or enter R to return.\n")
-        if self.asked_password == "r":
-            self.return_back(self.current_position)
-        else:
-            self.__is_password_correct(self.asked_password, password)
+            print("Wrong password, try again.")
+            return False
 
     def read_db_action(self):
         """Asks for db action"""        
-        self.current_position = "db_action"
         print("-"*50)
-        self.db_action = input(("Enter ADD to add new products;\n"
+        db_action = input(("Enter ADD to add new products;\n"
         "Enter DELETE to delete products;\n"
         "Enter UPDATE to change product properties.\n"
-        "Enter EARN to change your balance\n"
-        "Enter R to leave admin panel.\n")).strip().lower()
-        if self.db_action == "r":
-            self.return_back(self.current_position)
+        "Enter R to leave admin panel.\n"
+        "Please choose the option: ")).strip().lower()
+        if db_action == "r":
+            return "r"
+        elif self.__is_valid_db_action(db_action):
+            return db_action
         else:
-            self.__is_valid_db_action(self.db_action)
+            self.read_db_action()
     
     def __is_valid_db_action(self, db_action):
         """Validation of db action"""        
-        if db_action != "add" and db_action != "delete" and db_action != "update" and db_action != "earn":
-            self.db_action = input("Input is invalid, try again: ")
-            if self.db_action == "r":
-                self.return_back(self.current_position)
-            else:
-                self.__is_valid_db_action(self.db_action)
+        if db_action != "add" and db_action != "delete" and db_action != "update":
+            print("Input is invalid, try again.")
+            return False
+        else:
+            return True
 
     def __read_product_name(self):
         """Getting name of product"""    
         print("-"*50)   
-        self.chars["Name"] = input("Enter a name of the product: ")
+        return {"Name" : input("Enter a name of the product: ")}
     
     def __read_product_number(self):
         """Getting number of products"""        
         try:
             print("-"*50)
-            self.chars["Number"] = int(input("Enter a number of the product: "))
+            return {"Number" : int(input("Enter a number of the product: "))}
         except ValueError:
             print("-"*50)
             print("Invalid input, try again. ")
@@ -163,70 +154,64 @@ class Console:
         """Getting price of product"""        
         try:
             print("-"*50)
-            self.chars["Price"] = float(input("Enter a price of the product (Example: 329.99): "))
+            return {"Price" : float(input("Enter a price of the product (Example: 329.99): "))}
         except ValueError:
             print("-"*50)
             print("Invalid input, try again.")
             self.__read_product_price()
 
-    def read_product_chars(self):
+    def get_product_chars(self):
         """Returning created product"""
-        self.chars.clear()
-        self.__read_product_name()
-        self.__read_product_number()
-        self.__read_product_price()
+        return {**self.__read_product_name(), **self.__read_product_number(),
+        **self.__read_product_price()}
 
-    def get_product_id(self, category):
+    def get_product_id(self, category_len):
         """Getting product id for buying, deleting or updating"""
         print("-"*50)
-        self.id = input("Enter id of the product or R to return: ").strip().lower()
-        if self.id == "r":
-            self.return_back(self.current_position)     
+        id = input("Enter id of the product or R to return: ").strip().lower()
+        if id == "r":
+            return "r"     
         else:   
             try:
-                if self.id.isdigit():
-                    self.id = int(self.id)
-                if self.id >= len(category):
+                if id.isdigit():
+                    id = int(id)
+                if id >= category_len:
                     print("-"*50)
                     print("We don't have product with such id, try again.")
-                    self.get_product_id(category)
+                    self.get_product_id(category_len)
+                else:
+                    return id
             except TypeError:
                     print("-"*50)
                     print("Invalid input, try again.")
-                    self.get_product_id(category)
+                    self.get_product_id(category_len)
 
-    def get_number_of_product(self, category):
+    def get_number_of_product(self, db_number_of_product):
         """Getting number of products to buy""" 
         try:
             print("-"*50)
-            self.number = int(input("Enter number of the products: "))
-            if self.number > category[self.id]["Number"]:
+            number = int(input("Enter number of the products: "))
+            if number > db_number_of_product:
                 print("-"*50)
                 print("We don't have enough number, try again.")
-                self.get_number_of_product(category)
-            elif self.number == 0:
+                self.get_number_of_product(db_number_of_product)
+            elif number == 0:
                 print("-"*50)
                 print("You can't buy 0 products, try again.")
-                self.get_number_of_product(category)
+                self.get_number_of_product(db_number_of_product)
+            else:
+                return number
         except ValueError:
             print("-"*50)
             print("Invalid input, try again.")
-            self.get_number_of_product(category)
-
-    def return_back(self, position):
-        """Takes the user back"""        
-        match position:
-            case ("category_choosing" | "balance" | "password_check" 
-             | "password_creation" | "db_action" | "product_buying"):
-                self.read_user_action()
-            case "db_category_choosing":
-                self.read_db_action()
+            self.get_number_of_product(db_number_of_product)
 
     def refill_balance(self, balance):
         """Refilling balance"""
         try:
             print("-"*50)
             balance = int(input("Enter the number to top up the account(For example: 300): "))
+            return balance
         except ValueError:
             print("-"*50)
             print("Invalid input, try again.")
@@ -242,14 +227,13 @@ class Console:
             print("Price: ", product["Price"])
             print("-"*50)
 
-    def is_empty_category(self, category, db):
-        if len(category) == 0:
+    def is_empty_category(self, category_len):
+        if len(category_len) == 0:
             print("-"*50)
             print("This category is empty!")
-            self.get_category(db)
 
-    def availability_check(self, number, category):
-        if category[id]["Number"] - number < 0:
+    def availability_check(self, number, db_number_of_product):
+        if db_number_of_product - number < 0:
             print("-"*50)
             print("We don't have this number of products.")
             return False
